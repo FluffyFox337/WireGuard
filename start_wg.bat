e7655e63774ecf025bd01f007c35244dd3933570
~~~FluffyFox~~~ <84251725+FluffyFox337@users.noreply.github.com>
2024-10-14 16:12:11 +0300

    Update start_wg.bat

--- start_wg.bat
+++ start_wg.bat
@@ -3,7 +3,7 @@ set "/dp=%~dp0"
 set "/wg=C:\Program Files\WireGuard"
 :: set "name_exe_wg_installer=wireguard-amd64-0.5.3.msi"
 set "name_exe_wg=wireguard.exe"
-set "name_script_obtain=WG_obtain.ps1"
+set "name_script_obtain=wg_block_obtain.ps1"
 set "name_script_start_WG=start_wg.bat"
 
 powershell.exe -noprofile -executionpolicy bypass -file %name_script_obtain%
